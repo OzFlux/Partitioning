bfe3ea5d12cdeaf03e8d59acb1e041c0966d8d02
Ian McHugh <ian_mchugh@fastmail.com>
2019-08-15 15:55:03 +1000

    Updated to python3 - working!

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ Created on Tue Mar 20 11:20:56 2018
 def rename_df(df, external_names, internal_names):
     assert sorted(external_names.keys()) == sorted(internal_names.keys())
     swap_dict = {external_names[key]: internal_names[key] 
-                 for key in internal_names.keys()}
-    sub_df = df[swap_dict.keys()].copy()
-    sub_df.columns = swap_dict.values()
+                 for key in list(internal_names.keys())}
+    sub_df = df[list(swap_dict.keys())].copy()
+    sub_df.columns = list(swap_dict.values())
     return sub_df
